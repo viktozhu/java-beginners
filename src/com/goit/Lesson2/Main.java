739a28ca7380e6bb68b0211c03e1ab96620130f3
Kristina <kpe@ciklum.com>
2015-04-23 21:36:07 +0300

    Lesson1 added

---
+++ src/com/goit/Lesson2/Main.java
@@ -0,0 +1,11 @@
+package com.goit.Lesson2;
+
+/**
+ * Created by 1 on 23.04.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+   
+}
