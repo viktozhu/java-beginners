48d256749eac0c58e2029be8045e5d680e36577f
Viktozhu <viktor.zhurbenko@gmail.com>
2015-04-26 16:55:11 +0300

    scanner added

--- src/com/goit/Lesson1/Main.java
+++ src/com/goit/Lesson1/Main.java
@@ -1,8 +1,16 @@
 package com.goit.Lesson1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello world");
+        System.out.println("Hello world");
+
+        System.out.print("This text in row ");
+        System.out.println("Text in next row");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
     }
 }
